b19aea2edc8bfec7c0bf47e2d299450775ab4e73
ChaimaaN <chaimaanairi91@gmail.com>
2023-03-16 20:30:34 +0300

    commit

--- src/components/QAList.js
+++ src/components/QAList.js
@@ -2,6 +2,18 @@ import React from 'react'
 import { Row, Accordion } from 'react-bootstrap';
 import { question } from '../data'
 
+const QAList = ({ data, deleteOneItem }) => {
+
+const dataLocal = JSON.parse(localStorage.getItem("items"))
+//to delete one item from array
+const onDeleteItem = (ID) => {
+    if (localStorage.getItem("items") != null) {
+        const index = question.findIndex((item) => item.id === ID);
+        question.splice(index, 1)
+        deleteOneItem(question);
+    }
+}
+
 const QAList = () => {
   return (
     <div>
