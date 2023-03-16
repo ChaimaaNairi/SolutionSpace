4a15bbf60c71e650b78f7dcc81d7af3c27e3e550
ChaimaaN <chaimaanairi91@gmail.com>
2023-03-16 20:31:56 +0300

    commit

--- src/components/QAList.js
+++ src/components/QAList.js
@@ -3,23 +3,22 @@ import { Row, Accordion } from 'react-bootstrap';
 import { question } from '../data'
 
 const QAList = ({ data, deleteOneItem }) => {
+    const dataLocal = JSON.parse(localStorage.getItem("items"))
 
-const dataLocal = JSON.parse(localStorage.getItem("items"))
-//to delete one item from array
-const onDeleteItem = (ID) => {
-    if (localStorage.getItem("items") != null) {
-        const index = question.findIndex((item) => item.id === ID);
-        question.splice(index, 1)
-        deleteOneItem(question);
+    //to delete one item from array
+    const onDeleteItem = (ID) => {
+        if (localStorage.getItem("items") != null) {
+            const index = question.findIndex((item) => item.id === ID);
+            question.splice(index, 1)
+            deleteOneItem(question);
+        }
     }
-}
 
-const QAList = () => {
-  return (
+return (
     <div>
       
     </div>
   )
 }
 
-export default QAList
+export default QAList;
