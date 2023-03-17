0c8283ec3823bfd82d74612826b86cd4a1181203
ChaimaaN <chaimaanairi91@gmail.com>
2023-03-17 12:26:36 +0300

    commit

--- src/components/QAList.js
+++ src/components/QAList.js
@@ -15,10 +15,28 @@ const QAList = ({ data, deleteOneItem }) => {
     }
 
 return (
-    <div>
-      
-    </div>
-  )
+    <Row>
+    <Accordion>
+        {
+            localStorage.getItem("items") != null ? (dataLocal.map((item, index) => {
+                return (<Accordion.Item key={index} eventKey={item.id}>
+                    <Accordion.Header>
+                        <div className="m-auto">{item.q}</div>
+
+                    </Accordion.Header>
+                    <Accordion.Body className="text-end">
+                        <div className="px-3 d-inline ">{item.a}</div>
+                        <button onClick={() => onDeleteItem(item.id)} className="btn-color">مسح  </button>
+                    </Accordion.Body>
+                </Accordion.Item>)
+
+            })) : <h2 className="fs-4 text-center p-5">لا يوجد اسئله الان</h2>
+        }
+
+
+    </Accordion>
+</Row>
+)
 }
 
 export default QAList;
