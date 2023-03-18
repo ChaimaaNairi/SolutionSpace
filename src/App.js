5e9c450aaa5b460cac0c3af73a1ccaa28cdc83f2
ChaimaaN <chaimaanairi91@gmail.com>
2023-03-18 23:00:38 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,40 @@ import 'react-toastify/dist/ReactToastify.css';
 function App() {
   const [data, setData] = useState(question)
 
+   //to add new item
+   const addItem = () => {
+    localStorage.setItem("items", JSON.stringify([...question]));
+    setData([...question])
+    notify("  Added successfully", "Success")
+  }
+
+  //to delete all data items
+  const deleteAllItems = () => {
+    localStorage.removeItem("items")
+    question.splice(0, question.length);
+    setData([])
+    notify("تم حذف الكل بنجاح", "Success")
+  }
+
+  //to delete one item from array
+  const deleteOneItem = (items) => {
+    localStorage.setItem("items", JSON.stringify([...items]));
+    setData([...items])
+    notify("تم حذف السوال بنجاح", "Success")
+    if (items.length <= 0) {
+      deleteAllItems();
+    }
+  }
+
+    //to push notifaction 
+
+    const notify = (message, type) => {
+      if (type === "Error")
+        toast.error(message)
+      else if (type === "Success")
+        toast.success(message)
+    };
+  
   return (
     <div className="App">
       
